4ed3d85b1fe352ef51aadbd5c8953bd7a708a960
TobiasGSmollett <landofscala@gmail.com>
2020-07-25 12:28:25 +0900

    Adds test for enqueue on multi thread

--- spec/queue_spec.cr
+++ spec/queue_spec.cr
@@ -4,18 +4,41 @@ include Cavorite
 
 describe Cavorite do
 
-  it "works" do
+  it "single thread" do
 
     input = [1, 2, 3, 4, 5]
+    output = [] of Int32
 
     queue = Queue(Int32).new
 
-    input.each { |e| queue.enqueue e }
-    output = [] of Int32
+    input.each { |e| queue.enqueue(e) }
     until queue.empty?
-      tmp = queue.dequeue
-      output << tmp if !tmp.nil?
+      output << queue.dequeue.as(Int32)
     end
     input.should eq(output)
+  end
+
+  it "enqueue on multi thread" do
+    input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    wait_group = Array(Channel(Nil)).new(10, Channel(Nil).new)
+
+    queue = Queue(Int32).new
+
+    (1..10).each do |i|
+      spawn do
+        input.each { |e| queue.enqueue(e) }
+        wait_group[i-1].send(nil)
+      end
+    end
+
+    (0..9).each do |i|
+      wait_group[i].receive
+    end
+
+    sum = 0
+    until queue.empty?
+      sum += queue.dequeue.as(Int32)
+    end
+    sum.should eq(550)
   end
 end
